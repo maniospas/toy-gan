dddbad66ed2760d6407346651ee817b0c90dec9e
mkrasanakis <maniospas@hotmail.com>
2021-05-18 17:01:56 +0300

    more customization

--- test.py
+++ test.py
@@ -5,15 +5,19 @@ from matplotlib import pyplot as plt
 
 params = {"activation": tf.nn.leaky_relu, # recommended vs tf.nn.relu
           "optimizer": tf.optimizers.RMSprop(learning_rate=0.001), #tf.optimizers.Adam(learning_rate=0.001) does not converge
-          "noise": 0, # better robustness than zero (learn to predict second mode) but a little flipflopping
+          "noise": 0.1, # better robustness
+          "skip discriminator training": 1, # train discriminator every fixed number of epochs (1 does not skip training)
           "random variables": 2,
-          "batch_size": 512, # use a single batch
-          "dropout": 0.5
+          "batch_size": float('inf'), # use a single batch
+          "dropout": 0.5,
+          "loss": "Entropy" # Wasserstein or Entropy
         }
 
 np.random.seed(1)
-data = np.array([(x-0.5,0.7+(x-0.5)**2) for x in np.random.sample(200)]+[(x-1,2*(x-1)**3) for x in np.random.sample(200)])
+data = np.array([(x-0.5,0.7+(x-0.5)**2) for x in np.random.sample(400)]+[(x-1,2*(x-1)**3) for x in np.random.sample(40)])
 
+#plt.scatter(data[:, 0], data[:, 1])
+#plt.show()
 
 generator = tf.keras.Sequential([
     tf.keras.layers.Dense(16, activation=params["activation"], input_shape=(params["random variables"],)),
@@ -26,7 +30,7 @@ discriminator = tf.keras.Sequential([
     tf.keras.layers.Dropout(params["dropout"]),
     tf.keras.layers.Dense(16, activation=params["activation"]),
     tf.keras.layers.Dropout(params["dropout"]),
-    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)
+    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid if params["loss"] == "Entropy" else lambda x: x)
 ])
 
 # create dynamically updated figure
@@ -38,8 +42,7 @@ optimizer = params["optimizer"]
 batch_size = params["batch_size"]
 latent_random_variables = params["random variables"]
 epsilon = params["noise"]
-positive_labels = np.ones((batch_size, 1))
-negative_labels = np.zeros((batch_size, 1))
+visualization_random = tf.random.normal(shape=(200, latent_random_variables)) # visualize the same 200 examples each time
 for epoch in range(10001):
     if params["batch_size"] < data.shape[0]:
         np.random.shuffle(data)
@@ -50,24 +53,33 @@ for epoch in range(10001):
         random = tf.random.normal(shape=(batch_size, latent_random_variables))
         with tf.GradientTape() as tape:
             generated = generator(random)
-            prediction_negative = discriminator(generated+epsilon*tf.random.normal(shape=generated.shape))
-            loss = -tf.losses.BinaryCrossentropy()(negative_labels[:batch_size], prediction_negative)
+            prediction_negative = discriminator(generated+epsilon*tf.random.normal(shape=generated.shape), training=False)
+            if params["loss"] == "Wasserstein":
+                loss = tf.reduce_sum(prediction_negative)
+            else:
+                loss = tf.reduce_sum(tf.math.log(1-prediction_negative))
+            #loss = -tf.losses.BinaryCrossentropy()(negative_labels[:batch_size], prediction_negative)
         optimizer.apply_gradients(zip(tape.gradient(loss, generator.trainable_variables), generator.trainable_variables))
 
-        # train discriminator
-        random = tf.random.normal(shape=(batch_size, latent_random_variables))
-        with tf.GradientTape() as tape:
-            generated = generator(random)
-            batch_data = data[batch_pos:batch_pos+batch_size, :]
-            prediction_negative = discriminator(generated+epsilon*tf.random.normal(shape=generated.shape))
-            prediction_positive = discriminator(batch_data+epsilon*tf.random.normal(shape=batch_data.shape))
-            loss = tf.losses.MeanSquaredError()(negative_labels[:batch_size], prediction_negative) \
-                + tf.losses.MeanSquaredError()(positive_labels[:batch_size], prediction_positive)
-        optimizer.apply_gradients(zip(tape.gradient(loss, discriminator.trainable_variables), discriminator.trainable_variables))
+        if epoch % params["skip discriminator training"] == 0:
+            # train discriminator
+            random = tf.random.normal(shape=(batch_size, latent_random_variables))
+            with tf.GradientTape() as tape:
+                generated = generator(random)
+                batch_data = data[batch_pos:batch_pos+batch_size, :]
+                prediction_negative = discriminator(generated+epsilon*tf.random.normal(shape=generated.shape))
+                prediction_positive = discriminator(batch_data+epsilon*tf.random.normal(shape=batch_data.shape))
+                if params["loss"] == "Wasserstein":
+                    loss = -tf.reduce_sum(prediction_negative)+tf.reduce_sum(prediction_positive)
+                else:
+                    loss = -tf.reduce_sum(tf.math.log(1-prediction_negative))-tf.reduce_sum(tf.math.log(prediction_positive))
+                #loss = tf.losses.MeanSquaredError()(negative_labels[:batch_size], prediction_negative) \
+                #    + tf.losses.MeanSquaredError()(positive_labels[:batch_size], prediction_positive)
+            optimizer.apply_gradients(zip(tape.gradient(loss, discriminator.trainable_variables), discriminator.trainable_variables))
         batch_pos += batch_size
 
     if epoch % 10 == 0:
-        generated = generator(tf.random.normal(shape=(data.shape[0], latent_random_variables)))
+        generated = generator(visualization_random)
         axis.clear()
         axis.scatter(data[:, 0], data[:, 1])
         axis.scatter(generated[:, 0], generated[:, 1])
